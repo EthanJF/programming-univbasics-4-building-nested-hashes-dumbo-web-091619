ff1cb2e3c37a1ca68e4d503f809e46968180421a
comic-engineer-3780 <ethanjosephfournier@gmail.com>
2019-09-03 01:46:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,7 +28,7 @@ def monopoly_with_third_tier
 	    :pieces => 4,
 	    :rent_in_dollars => {
 	      :one_piece_owned => 25,
-	      :key2 => "value2",
+	      :two_pieces_owned => 50,
 	      :key3 => "value3",
 	      :key4 => "value4"
 	    },
